000d6bb0f1c9dae03a76703374051513763145d9
Fabio de Matos <fabio.dematos@butterfly.com.au>
2018-04-20 11:27:42 +1000

    Transitioning themes on same view

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,70 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  ThemeData _themeData;
+  String _debugMsg;
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+      theme: _themeData,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Title'),
+        ),
+        body: new Text(_debugMsg),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
+  void initState() {
+    // TODO: implement initState
+    super.initState();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    _shootThemeTransition();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void reassemble() {
+    super.reassemble();
+
+    _shootThemeTransition();
+  }
+
+  void _shootThemeTransition() {
+    _themeData = initialThemeData;
+    _debugMsg = 'First theme';
+    print(_debugMsg);
+
+    _scheduleThemeChange('Second theme', 5, secondThemeData);
+
+
+  }
+
+  _scheduleThemeChange(String msg, int delaySeconds, ThemeData theme){
+    new Timer(new Duration(seconds: delaySeconds), () {
+      setState(() {
+        _debugMsg = msg;
+        print(_debugMsg);
+        _themeData = theme;
+      });
     });
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
+
+var initialThemeData = new ThemeData(
+    primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.yellow);
+
+var secondThemeData = new ThemeData(
+    primarySwatch: Colors.red, scaffoldBackgroundColor: Colors.redAccent);
